46c72aec9f0670e4b51f8587f6553619028c49fc
MuXiu1997 <muxiu1997@gmail.com>
2023-12-04 16:32:31 +0800

    build: use @vercel/ncc build dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import packageJson from './package.json'
 
+// @ts-expect-error no types
+import ncc from '@vercel/ncc'
 import AutoImport from 'unplugin-auto-import/vite'
 import { defineConfig } from 'vite'
 
+import fs from 'node:fs'
 import path from 'node:path'
 
 export default defineConfig({
@@ -20,7 +23,7 @@ export default defineConfig({
     reportCompressedSize: false,
     lib: {
       entry: ['src/main.ts', 'src/post.ts'],
-      fileName: (_, entryName) => `${entryName}.mjs`,
+      fileName: (_, entryName) => `${entryName}.vite.mjs`,
       formats: ['es'],
     },
     rollupOptions: {
@@ -35,5 +38,26 @@ export default defineConfig({
       dirs: ['src/globals'],
       dts: true,
     }),
+    (() => {
+      const outputs: string[] = []
+      return {
+        name: 'rollup-plugin-ncc',
+        writeBundle(options, bundle) {
+          outputs.push(...Object.keys(bundle).map(file => path.join(options.dir!, file)))
+        },
+        async closeBundle() {
+          await Promise.all(
+            outputs.filter(file => file.endsWith('.vite.mjs'))
+              .map(async (file) => {
+                const { code } = await ncc(file, {
+                  minify: true,
+                })
+                await fs.promises.writeFile(file.replace(/\.vite\.mjs$/, '.mjs'), code)
+              }),
+          )
+          await Promise.all(outputs.map(file => fs.promises.rm(file)))
+        },
+      } as import('vite').Plugin
+    })(),
   ],
 })
